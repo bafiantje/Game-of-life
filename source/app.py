840ff239c99f79ddc72089fd6c32bd8f65e588cb
Fabian van Unen <171452@student.horizoncollege.nl>
2024-09-09 11:56:38 +0200

    removed blank, added icon, added icon to window

--- source/app.py
+++ source/app.py
@@ -4,6 +4,7 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 pygame.display.set_caption("Game of Life")
+pygame.display.set_icon(pygame.image.load("library/app_icon.png"))
 
 # With this loop the game will:
 # automatically draw all; 
