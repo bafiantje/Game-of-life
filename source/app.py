b214cf5dd3862dc3e5ecbfb2c9a35f357fd012c7
Fabian van Unen <171452@student.horizoncollege.nl>
2024-09-09 11:14:49 +0200

    initial release + added program that opens a window with pygame.

---
+++ source/app.py
@@ -0,0 +1,16 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+pygame.display.set_caption("Game of Life")
+
+# With this loop the game will:
+# automatically draw all; 
+# update everything.
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
