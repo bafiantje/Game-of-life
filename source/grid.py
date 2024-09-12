c1a081d6f9db5fab8fbaf1612c9d98ec97d35bd8
Fabian <deadlypixels@outlook.com>
2024-09-12 12:14:50 +0200

    Ages reset on randomization of grid

--- source/grid.py
+++ source/grid.py
@@ -25,13 +25,14 @@ class Grid:
         for row in range(self.rows):
             for column in range(self.columns):
                 self.cells[row][column] = random.choice([1, 0, 0, 0])
+                self.ages[row][column] = 0
                 
     def clear(self):
         for row in range(self.rows):
             for column in range(self.columns):
                 self.cells[row][column] = 0
+                self.ages[row][column] = 0
                 
     def toggle_cell(self, row, column):
         if 0 <= row < self.rows and 0 <= column < self.columns:
             self.cells[row][column] = not self.cells[row][column]  
-        
